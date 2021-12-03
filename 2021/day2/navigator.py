860e5eae25d9232cb4335be0777551ea6767ba71
Cameron Walker <whatiscameronsemail@gmail.com>
2021-12-03 10:52:13 -0700

    cleaning up code

--- 2021/day2/navigator.py
+++ 2021/day2/navigator.py
@@ -1,19 +1,27 @@
-from typing import Match
-
-
 from movements import *
+
+# movements = '''forward 5
+# down 5
+# forward 8
+# up 3
+# down 8
+# forward 2'''
+
+
 
 def calculate_movement(movements):
 	h_pos = 0
 	depth = 0
+	aim = 0
 	for movement in movements.split('\n'):
 		[direction, magnitude] = movement.split(' ')
 		if direction == 'forward':
 			h_pos += int(magnitude)
+			depth += (aim * int(magnitude))
 		elif direction == 'down':
-			depth += int(magnitude)
+			aim += int(magnitude)
 		elif direction == 'up':
-			depth -= int(magnitude)
+			aim -= int(magnitude)
 	
 	return [h_pos, depth, h_pos * depth]
 	
--- 2021/day3/day3.py
+++ 2021/day3/day3.py
@@ -1,6 +1,6 @@
 from input import *
 
-def binaryToDecimal(n):
+def binary_to_decimal(n):
   return int(n,2)
 
 def count_bits(inputs):
@@ -9,17 +9,17 @@ def count_bits(inputs):
 	for digit in inputs:
 		for i in range(len(digit)):
 			if (i not in bit_map):
-				bit_map[i] = { '1':0, '0':0 }
+				bit_map[i] = { '1': 0, '0': 0 }
 			bit_map[i][digit[i]] += 1
 	
 	return bit_map
 
-def get_prime_bit(bit_map, index):
+def get_common_bit(bit_map, index):
 	if bit_map[index]['0'] > bit_map[index]['1']:
 		return '0'
 	return '1'
 
-def get_unprime_bit(bit_map, index):
+def get_uncommon_bit(bit_map, index):
 	if bit_map[index]['0'] <= bit_map[index]['1']:
 		return '0'
 	return '1'
@@ -29,7 +29,7 @@ def last_dig_rem(index, values, func):
 	filtered_vals = list(filter(lambda val: val[index] == key, values))
 	if len(filtered_vals) > 1:
 		return last_dig_rem(index + 1, filtered_vals, func)
-	return binaryToDecimal(filtered_vals[0])
+	return binary_to_decimal(filtered_vals[0])
 
 def calculate_power_cons(input):
 	inputs = input.split('\n');
@@ -39,15 +39,15 @@ def calculate_power_cons(input):
 	epsilon = ''
 
 	for index in bit_map:
-		gamma += get_prime_bit(bit_map, index)
-		epsilon += get_unprime_bit(bit_map, index)
+		gamma += get_common_bit(bit_map, index)
+		epsilon += get_uncommon_bit(bit_map, index)
 
 	print(gamma)
 	print(epsilon)
 
 	
-oxy = last_dig_rem(0, test_input.split('\n'), get_prime_bit)
-co2 = last_dig_rem(0, test_input.split('\n'), get_unprime_bit)
+oxy = last_dig_rem(0, test_input.split('\n'), get_common_bit)
+co2 = last_dig_rem(0, test_input.split('\n'), get_uncommon_bit)
 print(oxy)
 print(co2)
 print(oxy*co2)
